1ff3ce1515d06d30c55240d6475112e7db6544c6
SiddhikaDR <siddhika2468@gmail.com>
2024-08-29 10:32:25 +0530

    Add files via upload

---
+++ ContactManager.java
@@ -0,0 +1,41 @@
+package CaseStudy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactManager {
+
+	static List l,l2;
+	public ContactManager()
+	{
+		l=new ArrayList();
+		l.add("java");
+		l.add("c++");
+		l.add("python");
+	}
+	public void addContact(String a)
+	{
+		l.add(a);
+	}
+	public List getContact()
+	{
+		return l;
+		//l.forEach(x->System.out.print(x+" "));	
+	}
+	public void print1()
+	{
+		l2.forEach(x->System.out.print(x+" \n"));
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ContactManager c=new ContactManager();
+		c.addContact("aadi");
+		l2=c.getContact();
+		System.out.println(l2);
+		System.out.println("\nprinting using for each");
+		c.print1();
+
+	}
+
+}
